e28da27d3b8588d252f1e45dfe035129b7ccbba4
actuallyfro <actuallyfro@github.com>
2023-03-28 00:12:07 -0500

    {main_menu} Refactoring

--- src/scripts/Main-menu.gd
+++ src/scripts/Main-menu.gd
@@ -1,17 +1,12 @@
 extends Node
 
-# global variable for muteTogle
 var isMuted = false
-
-# Array for scenes
-var GameStages = ["MainMenu", "Game", "GameOver", "PauseMenu"]
+#var GameStages = ["MainMenu", "Game", "GameOver", "PauseMenu"]
 var MainMenuOptions = ["Play", "Options", "Quit"]
 var menuMenuChoice = 0
 
-var currentStage = GameStages[0]
 var isMenuChanged = false
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
@@ -26,53 +21,34 @@ func _input(event):
 			isMuted = !isMuted
 
 	if event.is_action_pressed("quit"):
-		# Quit if at main menu
-		if currentStage == "MainMenu":
-			get_tree().quit()
+		get_tree().quit()
 
 	if event.is_action_pressed("arrow_up"):
-		if currentStage == "MainMenu":
-			menuMenuChoice -= 1
-			if menuMenuChoice < 0:
-				menuMenuChoice = 2
-			isMenuChanged = true
+		menuMenuChoice -= 1
+		if menuMenuChoice < 0:
+			menuMenuChoice = 2
+		isMenuChanged = true
+			
 			
 	if event.is_action_pressed("arrow_down"):
-		if currentStage == "MainMenu":
-			menuMenuChoice += 1
-			if menuMenuChoice > 2:
-				menuMenuChoice = 0
-			isMenuChanged = true
+		menuMenuChoice += 1
+		if menuMenuChoice > 2:
+			menuMenuChoice = 0
+		isMenuChanged = true
 
 	if isMenuChanged:
-		if currentStage == "MainMenu":
-			$MenuOptions/VBoxContainer/Play.add_theme_color_override("font_color",Color(1,1,1))
-			$MenuOptions/VBoxContainer/Options.add_theme_color_override("font_color",Color(1,1,1))
-			$MenuOptions/VBoxContainer/Quit.add_theme_color_override("font_color",Color(1,1,1))
+		$MenuOptions/VBoxContainer/Play.add_theme_color_override("font_color",Color(1,1,1))
+		$MenuOptions/VBoxContainer/Options.add_theme_color_override("font_color",Color(1,1,1))
+		$MenuOptions/VBoxContainer/Quit.add_theme_color_override("font_color",Color(1,1,1))
 
-			match menuMenuChoice:
-				0:
-					$MenuOptions/VBoxContainer/Play.add_theme_color_override("font_color",Color(1,0,0))
-				1:
-					$MenuOptions/VBoxContainer/Options.add_theme_color_override("font_color",Color(1,0,0))
-				2:
-					$MenuOptions/VBoxContainer/Quit.add_theme_color_override("font_color",Color(1,0,0))
-
-			# $MenuOptions/VBoxContainer/Play.label_settings.them
-			# $Main-menu/MenuOptions/VBoxContainer/Options.font_color = Color(1,1,1)
-			# $Main-menu/MenuOptions/VBoxContainer/Quit.font_color = Color(1,1,1)
-			
-			# switch
-			# match menuMenuChoice:
-			# 	0:
-			# 		$Main-menu/MenuOptions/VBoxContainer/Play.font_color = Color(1,0,0)
-			# 	1:
-			# 		$Main-menu/MenuOptions/VBoxContainer/Options.font_color = Color(1,0,0)
-			# 	2:
-			# 		$Main-menu/MenuOptions/VBoxContainer/Quit.font_color = Color(1,0,0)
-				# else:			
+		match menuMenuChoice:
+			0:
+				$MenuOptions/VBoxContainer/Play.add_theme_color_override("font_color",Color(1,0,0))
+			1:
+				$MenuOptions/VBoxContainer/Options.add_theme_color_override("font_color",Color(1,0,0))
+			2:
+				$MenuOptions/VBoxContainer/Quit.add_theme_color_override("font_color",Color(1,0,0))
 		isMenuChanged = false
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
