d9fb6b29cebb33d9a20f46cdd00450bb207bdafe
actuallyfro <actuallyfro@github.com>
2023-03-27 22:43:30 -0500

    {main_menu} Adding Quit at main button

--- src/scripts/Main-menu.gd
+++ src/scripts/Main-menu.gd
@@ -3,6 +3,10 @@ extends Node
 # global variable for muteTogle
 var isMuted = false
 
+# Array for scenes
+var GameStages = ["MainMenu", "Game", "GameOver", "PauseMenu"]
+var currentStage = GameStages[0]
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -16,6 +20,11 @@ func _input(event):
 		else:
 			AudioServer.set_bus_mute(0, true)
 			isMuted = !isMuted
+
+	if event.is_action_pressed("quit"):
+		# Quit if at main menu
+		if currentStage == "MainMenu":
+			get_tree().quit()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
