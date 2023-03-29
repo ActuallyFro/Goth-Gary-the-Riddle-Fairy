fc6a3a092b1a03e5861c34aedee5799fbe3923a0
actuallyfro <actuallyfro@github.com>
2023-03-28 23:07:46 -0500

    {playing} Dynamic loading works! issues with case sensitivity and input field

--- src/scripts/playing.gd
+++ src/scripts/playing.gd
@@ -22,47 +22,49 @@ func loadJSON():
 #		saveJSON()
 		return
 	var file = FileAccess.open("res://resources/answers/Chapter1.csv", FileAccess.READ)
-#	# print file status
-#	if file.get_error() != OK:
-#		print("Error: " + str(file.get_error()))
-#		return
-#	else:
-#		print("File opened successfully")
+
+	if file.get_error() != OK:
+		print("[ERROR] for loadJSON(): " + str(file.get_error()))
+		return
+	else:
+		print("[DEBUG] File opened successfully")
 #
 	file_data = JSON.parse_string(file.get_as_text())
-#	# print file_data
-	# print(data)
-	# Print total objects in file_data
-	print("[DEBUG] Total objects in file_data: " + str(file_data.size()))
-	# iterate over all objects in file_data
-	for i in file_data:
-		print("[DEBUG] Object " + str(i) + ": " + str(file_data[i]))
-#		current_question=str(file_data[0].question)
-#		current_answer=str(file_data[0].answer)
-#		print("[DEBUG] Current question: " + current_question)
-#		print("[DEBUG] Current answer: " + current_answer)
-
-#	file_data = data
 
 func _ready():
 	loadJSON()
 
-	#	if file_data.tutorial.has("name"):
-	# print(file_data)
+#	print("[DEBUG] Total objects in file_data: " + str(file_data.size()))
+	var count=0
+	for i in file_data:
+		if count == current_index:
+			for j in file_data[i]:
+				if j == "question":
+					current_question = str(file_data[i][j])
+				if j == "answer":
+					current_answer = str(file_data[i][j])
+			break
+		count += 1
+#	print("[DEBUG] current_question " + current_question)
+#	print("[DEBUG] current_answer " + current_answer)
 
+	$RiddleArea/Question.text = current_question
 	$RiddleArea/Input.grab_focus()
-
-	# var Chpt1_QandA = GetChapter1QandA()
-#	var file = FileAccess.open("res://resources/answers/Chapter1.csv", FileAccess.READ)
 
 
 
 func _input(event):
 	if event.is_action_pressed("enter"):
-		$RiddleArea/Input.text = $RiddleArea/Answer.text.replace("\n", "")
-		$RiddleArea/Input.text = $RiddleArea/Answer.text.replace("\r", "")
+		var userInput = $RiddleArea/Input.text
+		userInput = userInput.replace("\n", "")
+		if userInput == current_answer:
+			$RiddleArea/Answer.text = current_answer
+		else:
+			$RiddleArea/Answer.text = "Try Again..."
+#		$RiddleArea/Input.text = $RiddleArea/Input.text.replace("\n", "")
+#		$RiddleArea/Input.text = $RiddleArea/Input.text.replace("\r", "")
 #		$RiddleArea/Answer.text = $RiddleArea/Answer.text = " "
-		$RiddleArea/Answer.text = "Darkness"
+		
 
 	if event.is_action_pressed("quit"):
 		get_tree().change_scene_to_file("res://src/scenes/main_nenu.tscn")
