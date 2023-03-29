7414bd54ce97470846dbf1dc3d3738a0395dac5d
actuallyfro <actuallyfro@github.com>
2023-03-28 22:50:02 -0500

    {playing} Trying to load QandA from file...

--- src/scripts/playing.gd
+++ src/scripts/playing.gd
@@ -1,10 +1,61 @@
 extends Node
 
+#var file_data = {
+#	"tutorial" : {
+#		"name": "..."
+#	}    
+#}
 
-# Called when the node enters the scene tree for the first time.
+var file_data
+var current_question
+var current_answer
+var current_index = 0
+
+#func saveJSON():
+#	var file = FileAccess.open("res://resources/answers/Chapter1.csv", FileAccess.WRITE)
+#	var json = JSON.new()
+#	var json_string = json.stringify(file_data, "  ") 
+#	file.store_line(json_string)
+
+func loadJSON():
+	if not FileAccess.file_exists("res://resources/answers/Chapter1.csv"):
+#		saveJSON()
+		return
+	var file = FileAccess.open("res://resources/answers/Chapter1.csv", FileAccess.READ)
+#	# print file status
+#	if file.get_error() != OK:
+#		print("Error: " + str(file.get_error()))
+#		return
+#	else:
+#		print("File opened successfully")
+#
+	file_data = JSON.parse_string(file.get_as_text())
+#	# print file_data
+	# print(data)
+	# Print total objects in file_data
+	print("[DEBUG] Total objects in file_data: " + str(file_data.size()))
+	# iterate over all objects in file_data
+	for i in file_data:
+		print("[DEBUG] Object " + str(i) + ": " + str(file_data[i]))
+#		current_question=str(file_data[0].question)
+#		current_answer=str(file_data[0].answer)
+#		print("[DEBUG] Current question: " + current_question)
+#		print("[DEBUG] Current answer: " + current_answer)
+
+#	file_data = data
+
 func _ready():
+	loadJSON()
+
+	#	if file_data.tutorial.has("name"):
+	# print(file_data)
+
 	$RiddleArea/Input.grab_focus()
-	pass
+
+	# var Chpt1_QandA = GetChapter1QandA()
+#	var file = FileAccess.open("res://resources/answers/Chapter1.csv", FileAccess.READ)
+
+
 
 func _input(event):
 	if event.is_action_pressed("enter"):
