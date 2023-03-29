82bd429661615d1b8996cbee775b59da812971a9
actuallyfro <actuallyfro@github.com>
2023-03-28 21:55:16 -0500

    {playing} Spliting input and answer; adding flair to answer

--- src/scripts/playing.gd
+++ src/scripts/playing.gd
@@ -3,19 +3,18 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$RiddleArea/Input.grab_focus()
+	pass
 
 func _input(event):
 	if event.is_action_pressed("enter"):
-#		$RiddleArea/Answer.text = $RiddleArea/Answer.text.replace("\n", "")
+		$RiddleArea/Input.text = $RiddleArea/Answer.text.replace("\n", "")
+		$RiddleArea/Input.text = $RiddleArea/Answer.text.replace("\r", "")
 #		$RiddleArea/Answer.text = $RiddleArea/Answer.text = " "
 		$RiddleArea/Answer.text = "Darkness"
 
 	if event.is_action_pressed("quit"):
 		get_tree().change_scene_to_file("res://src/scenes/main_nenu.tscn")
 
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+#func process(delta):
+#	pass
